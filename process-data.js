502ad43fdad4395a89b15fa2e1381edf501d0930
abbyfisher <abbyfisher1121@gmail.com>
2016-08-07 11:17:09 -0600

    added comments to gain better knowledge of the code
    :x

--- process-data.js
+++ process-data.js
@@ -1,154 +1,167 @@
 //import data and library
-var data = require('./data-exports/export5.js'),
-	changeData = require('./clean-change-data.js'),
-	natural = require('natural'),
-	tokenizer = new natural.WordTokenizer(),
-	stemmer = natural.LancasterStemmer,
-	stopwords = require('stopwords').english,
-	ignore  = ['aapl', '500', 'month', 'september', 'exxonmoble', 'monday', 'july,', 'friday', 'nysewfc', 'google', 'two', 'york', 'buffet', 'iphone', 'today', 'week', 'nyse', 'market', 'analyst', 'report', 'exxonmoble', 'among', 'brkb', '30', 'chevron', 'second', 'nysexom', 'google', 'four', 'verizon', 'xbox', 'thursday', 'wednesday', 'nysejpm', 'appl', 'apple', 'msft', 'microsoft', 'xom', 'exxon', 'jnj', 'johnson', 'ge', 'general', 'electric', 'wfc', 'wells', 'fargo', 'brk', 'berkshire', 'hathaway', 'berkshir', 'jp', 'when', 'say', 'firm', 'nysejnd', 'nysepf', 'nasdaq', 'nasdaqaapl', 'nasdaqmsft', '2015', 'it', 'corp', 'at', 'jpm', 'morgan', 'at%26t', 'at&t', 'pfe', 'pfizer', 'pfiz', 'company', 'jpmorgan', 'jpmorg'];
+
+
+// data is a export file within data-exports directory and it was  imported from the firebase database.
+var data = require( './data-exports/export4.js' ),
+
+//change data is the clean-change-data file that contains an object of date's for each of the queries
+    changeData = require( './clean-change-data.js' ),
+
+//"Natural" is a general natural language facility for nodejs. Tokenizing, stemming, classification, phonetics, tf-idf,
+    natural = require( 'natural' ),
+    tokenizer = new natural.WordTokenizer(),
+    stemmer = natural.LancasterStemmer,
+//stop-words are words which do not contain important significance to be used in Search Queries
+    stopwords = require( 'stopwords' ).english,
+    ignore = [ 'aapl', '500', 'month', 'september', 'exxonmoble', 'monday', 'july,', 'friday', 'nysewfc', 'google', 'two', 'york', 'buffet', 'iphone', 'today', 'week', 'nyse', 'market', 'analyst', 'report', 'exxonmoble', 'among', 'brkb', '30', 'chevron', 'second', 'nysexom', 'google', 'four', 'verizon', 'xbox', 'thursday', 'wednesday', 'nysejpm', 'appl', 'apple', 'msft', 'microsoft', 'xom', 'exxon', 'jnj', 'johnson', 'ge', 'general', 'electric', 'wfc', 'wells', 'fargo', 'brk', 'berkshire', 'hathaway', 'berkshir', 'jp', 'when', 'say', 'firm', 'nysejnd', 'nysepf', 'nasdaq', 'nasdaqaapl', 'nasdaqmsft', '2015', 'it', 'corp', 'at', 'jpm', 'morgan', 'at%26t', 'at&t', 'pfe', 'pfizer', 'pfiz', 'company', 'jpmorgan', 'jpmorg' ];
 
 //hooks library functions to string prototype
-stemmer.attach(); 
+stemmer.attach();
 
-console.time('Combine Strings');
-//combine all data for each row into one string
+console.time( 'Combine Strings' );
+
 var combined = '';
 
-//data is the export5 file within data-exports looks like the data was imported into this program from the firebase database. 
-for(d in data){
-	if(data[d].symbol != 'brk') {
-		var headlines = data[d].headlines.join();
-		var bodies = data[d].bodies.join();
-		var str = [].concat(headlines, bodies).toString().replace(/[^\w\s]|_/g, "").replace(/\s+/g, " ").toLowerCase();
-		combined += str;
-	}
+//combine all data for each row into one string using the headings and body of the data in exports into combined
+for( d in data ){
+    if( data[ d ].symbol != 'brk' ){
+        var headlines = data[ d ].headlines.join();
+        var bodies = data[ d ].bodies.join();
+        var str = [].concat( headlines, bodies ).toString().replace( /[^\w\s]|_/g, "" ).replace( /\s+/g, " " ).toLowerCase();
+        combined += str;
+    }
 }
-console.timeEnd('Combine Strings');
+console.timeEnd( 'Combine Strings' );
+
+console.time( 'Clean & Tokenize' );
+
+//remove stop words from the combined strings
+
+combined = combined.split( ' ' ).filter( function( word ){
+    return (!~stopwords.indexOf( word )) || (!~ignore.indexOf( word ))
+} ).join( ' ' );
 
 
-console.time('Clean & Tokenize');
-//remove stop words
-
-combined = combined.split(' ').filter(function(word){return (!~stopwords.indexOf(word))||(!~ignore.indexOf(word))}).join(' ');
-
-//combined is the string that is created from the bodies and headlines within 
-// he exports file being combined into one large sting with all of the stopwords taken out
-
-
-//create an array of tokens
+//create an array of tokens from combined 
 var tokens = combined.tokenizeAndStem();
 
-//the tokens are the strings broken up into an array 
-console.timeEnd('Clean & Tokenize');
+//the tokens are the strings broken up into an array
+console.timeEnd( 'Clean & Tokenize' );
 
-console.time('Count and sort Tokens');
-//count occurances of unique tokens
+console.time( 'Count and sort Tokens' );
+
+//count occurrences of unique tokens (being the words from the combined string)
 var counts = {};
-tokens.forEach(function(x) { counts[x] = (counts[x] || 0)+1; });
+tokens.forEach( function( x ){
+    counts[ x ] = (counts[ x ] || 0) + 1;
+} );
 
 
-// transform into a array of counted tokens
-// and remove ignore words
+// transform into a array of counted tokens and remove ignore words
+// creates an array of objects that have the words after removing ignore words and their count value
 var countsArray = [];
-for(c in counts){
-	if(ignore.indexOf(c) < 0){
-		countsArray.push({key: c, value: counts[c]});
-	}
+for( c in counts ){
+    if( ignore.indexOf( c ) < 0 ){
+        countsArray.push( { key: c, value: counts[ c ] } );
+    }
 }
 
 
-//sorted to find highest count and to remove upper and lower tokens
-countsArray.sort(function(a,b) {
-  if (a.value < b.value)
-    return -1;
-  if (a.value > b.value)
-    return 1;
-  return 0;
-});
+// sorted to find highest count and to remove upper and lower tokens
+// will give better result to remove the highest and lowest occurring words
+countsArray.sort( function( a, b ){
+    if( a.value < b.value )
+        return -1;
+    if( a.value > b.value )
+        return 1;
+    return 0;
+} );
 
-console.timeEnd('Count and sort Tokens');
+
+console.timeEnd( 'Count and sort Tokens' );
 
 
-console.time('Select Tokens');
+console.time( 'Select Tokens' );
 //Remove the highest and least used tokens
 var percent = 0.05,
-	min = 0,
-	max = countsArray[countsArray.length-1].value,
-	lowerBound = Math.floor(max * percent),
-	upperBound = Math.floor(max - (max*percent));
+    min = 0,
+    max = countsArray[ countsArray.length - 1 ].value,
+    lowerBound = Math.floor( max * percent ),
+    upperBound = Math.floor( max - (max * percent) );
 
-filtered = countsArray.filter(function(value){
-	return value.value > lowerBound && value.value < upperBound;
-});
+filtered = countsArray.filter( function( value ){
+    return value.value > lowerBound && value.value < upperBound;
+} );
+
 
 var dict = [];
-filtered.forEach(function(d){
-	dict.push(d.key);
-});
+filtered.forEach( function( d ){
+    dict.push( d.key );
+} );
 
 //dict is the new array of words sorted by use after the highest and lowest used words were taken out
-console.timeEnd('Select Tokens');
+console.timeEnd( 'Select Tokens' );
 
-console.time('Build Training Data');
+
+//build the training data that will be used for the neural network 
+console.time( 'Build Training Data' );
 var trainingData = [];
 
+//using each thing in data again
+for( e in data ){
+    var entry = data[ e ];
 
-for(e in data) {
-	var entry = data[e];
+    var signature = [];
 
-	var signature = [];
-	var sigSource = [].concat(entry.headlines, entry.bodies).toString().replace(/[^\w\s]|_/g, "").replace(/\s+/g, " ").toLowerCase().tokenizeAndStem();
-	
-	var features = [];
-	
-	//it is checking if d is within sigSource and if it is it pushed 1 into signature and d into features other wise it pushes 0 into signiture 
-	dict.forEach(function (d) {
-		if (sigSource.indexOf(d) > -1) {
-			signature.push(1);
-			features.push(d);
-		}
-		else {
-			signature.push(0)
-		}
-	});
+    //sigSource is a new array created by combining two things to one array
+    // it is crated with the headlines and bodies being converted to lowercase and tokenize to the base words including 
+    // stop words, ignored, and the highest and lowest occurring words
 
-	
+    //example  console.log("i am waking up to the sounds of chainsaws".tokenizeAndStem()); will return [ 'wak', 'sound', 'chainsaw' ]
 
-	var symbol = entry.symbol;
-	var date = entry.date;
-	var change = changeData[symbol][date];
+    var sigSource = [].concat( entry.headlines, entry.bodies ).toString().replace( /[^\w\s]|_/g, "" ).replace( /\s+/g, " " ).toLowerCase().tokenizeAndStem();
+    var features = [];
 
-	//it uses change to see if the symbol and date are within the clean and change data file if the entry is
-	//found and has a 0 or 1 is pushes the signature into training data that will be used to create trianing.js
+    // it is checking if a word from dict within sigSource and if it is, it pushes 1 into signature and d into features
+    // other wise it pushes 0 into signature 
+    dict.forEach( function( d ){
+        if( sigSource.indexOf( d ) > -1 ){
+            signature.push( 1 );
+            features.push( d );
+        }
+        else{
+            signature.push( 0 )
+        }
+    } );
 
-	if(change === 1 || change === 0){
-		trainingData.push({
-			input: signature,
-			output: { 'direction': change}
-		});
-	}
+    var symbol = entry.symbol;
+    var date = entry.date;
+
+    var change = changeData[ symbol ][ date ];
+
+    //it uses change to see if the symbol and date are within the clean and change data file if the entry is
+    //found and has a 0 or 1 is pushes the signature into training data that will be used to create trianing.js
+
+    if( change === 1 || change === 0 ){
+        trainingData.push( {
+            input: signature,
+            output: { 'direction': change }
+        } );
+    }
 }
 
-console.timeEnd('Build Training Data');
+console.timeEnd( 'Build Training Data' );
 
-console.time('Write File');
+console.time( 'Write File' );
 
 
-require('fs').writeFile(
-
-	'./training.js',
-
-	JSON.stringify(trainingData),
-	
-
-	function (err) {
-		if (err) {
-			console.error('Crap happens');
-		}
-	}
-	
+require( 'fs' ).writeFile( './training.js', JSON.stringify( trainingData ), function( err ){
+        if( err ){
+            console.error( 'Crap happens' );
+        }
+    }
 );
 
-console.timeEnd('Write File');
+console.timeEnd( 'Write File' );
 
 //added comments by Abby Fisher
--- scrape-data.js
+++ scrape-data.js
@@ -1,7 +1,9 @@
 //imports and app vars
 var request = require( 'request' ),
     cheerio = require( 'cheerio' ),
-    queries = [ 'AAPL', 'MSFT', 'XOM', 'JNJ', 'GE', 'WFC', 'BRK', 'JPM', 'AT%26T', 'PFE' ],
+//these queries are the top ten stock markets
+    queries = [ 'XOM' , 'MSFT', 'XOM', 'AAPL', 'JNJ', 'WFC', 'BRK', 'JPM', 'AT%26T', 'PFE', 'GE' ],
+//database used for storing the results of the scrap on google news
     Firebase = require( 'firebase' ),
     db = new Firebase( 'https://torrid-inferno-8475.firebaseio.com/' );
 queryQueue = [];
@@ -9,28 +11,35 @@ delay = 3000;
 UA = [ 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/600.8.9 (KHTML, like Gecko) Version/8.0.8 Safari/600.8.9', 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36', 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.0', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36', 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:40.0) Gecko/20100101 Firefox/40.0', 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.93 Safari/537.36', 'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko' ];
 
 
-//build list of queryies
-for( var month = 1; month <= 11; month++ ){
-    for( var day = 1; day <= 30; day++ ){
-        if( month === 1 && day >= 23 ){ //do partial months
+//build list of queries
+var year = 2016;
+
+for( var month = 1; month <= 1; month++ ){
+    for( var day = 1; day <= 10; day++ ){
+        if( month === 1 && day >= 10 ){ //do partial months
             queries.forEach( function( term ){
                 var ua = UA[ Math.floor( (Math.random() * 5) ) + 1 ];
                 queryQueue.push( {
                     term: term,
-                    date: month + '/' + day + '/' + '2015',
+                    date: month + '/' + day + '/' + year,
                     options: {
-                        url: 'https://www.google.com/search?q=' + term + '&hl=en&gl=us&authuser=0&biw=939&bih=1082&source=lnt&tbs=cdr%3A1%2Ccd_min%3A' + month + '%2F' + day + '%2F2015%2Ccd_max%3A' + month + '%2F' + day + '%2F2015&tbm=nws',
+                        url: 'https://www.google.com/search?q=' + term + '&hl=en&gl=us&authuser=0&biw=939&bih=1082&source=lnt&tbs=cdr%3A1%2Ccd_min%3A' + month + '%2F' + day + '%2F'+ year +'%2Ccd_max%3A' + month + '%2F' + day + '%2F'+year+'&tbm=nws',
                         headers: {
                             'User-Agent': ua
                         }
                     }
                 } );
             } );
+          
         }
     }
 }
 
+
+
 queryQueue.reverse();
+
+//uses each query to search google news and store the results into firebase
 
 function doQuery( query ){
     request( query.options, function( error, response, body ){
@@ -76,3 +85,6 @@ function doQuery( query ){
 }
 
 doQuery( queryQueue.pop() );
+
+
+//added comments by Abby Fisher
